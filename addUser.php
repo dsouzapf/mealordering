c37f3a3258e5858444b8ff59de86274171159a1b
dsouzapf <dsouza.pf@oundleschool.org.uk>
2020-10-14 09:56:16 +0100

    Prepare add user

---
+++ addUser.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Add User</title>
+    </head>
+
+    <body>
+
+        <form action="addUserRun.php">
+
+            Username:<input type="text" name="username"><br>
+            Initial Password:<input type="text" name="password"><br>
+
+            <input type="submit">
+
+        </form>
+        
+    </body>
+
+</html>
