7a9a3066c2b200a31db9cfa4fbdad25e1457e231
dsouzapf <dsouza.pf@oundleschool.org.uk>
2020-10-17 08:58:01 +0100

    Move password generation to new script

--- addUserRun.php
+++ addUserRun.php
@@ -13,21 +13,7 @@ array_map("htmlspecialchars", $_POST);
 
 include_once("connection.php");
 
-function generatePasswordFromSeed($seed) {
-
-    srand($seed);
-
-    $output = "";
-
-    for ($i = 0; $i < 8; $i++) {
-
-        $output .= chr(rand(65,122));
-
-    }
-
-    return $output;
-
-}
+include_once("passwordGeneration.php");
 
 $username = $_POST["username"];
 $passwordSeed = $_POST["passwordSeed"];
